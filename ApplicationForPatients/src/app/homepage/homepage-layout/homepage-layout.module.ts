e94dbfe3249c57f5281e6563e22421d3349ddd47
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-11-16 06:51:07 +0100

    feat(medical_record): Added fixed front

--- ApplicationForPatients/src/app/homepage/homepage-layout/homepage-layout.module.ts
+++ ApplicationForPatients/src/app/homepage/homepage-layout/homepage-layout.module.ts
@@ -6,6 +6,7 @@ import { ToastrModule } from "ngx-toastr";
 import { NavbarComponent } from "../components/navbar/navbar.component";
 import { SidebarComponent } from "../components/sidebar/sidebar.component";
 import { FeedbackComponent } from "../feedback/feedback.component";
+import { MedicalrecordsComponent } from "../medicalrecords/medicalrecords.component";
 import { ViewfeedbackComponent } from "../viewfeedback/viewfeedback.component";
 import { HomepageLayoutRoutes } from "./homepage-layout.routing";
 
@@ -22,12 +23,14 @@ import { HomepageLayoutRoutes } from "./homepage-layout.routing";
         NavbarComponent,
         SidebarComponent,
         FeedbackComponent,
-        ViewfeedbackComponent
+        ViewfeedbackComponent,
+        MedicalrecordsComponent
     ],
     exports: [
         NavbarComponent,
         SidebarComponent,
         FeedbackComponent,
+        
         
     ]
 })
