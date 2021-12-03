abbb5d8da81e6c61dcb01998c2d8461a17ec434b
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-12-03 03:39:13 +0100

    feat(medicalRecord) : Finished feature

---
+++ ApplicationForPatients/src/app/models/patient.ts
@@ -0,0 +1,24 @@
+import { Allergy } from "./Allergy";
+import { Doctor } from "./Doctor";
+
+export class Patient {
+    Name:string ;
+    Surname:string;
+    BirthDate : Date;
+    Jmbg: string;
+    Address:string;
+    Phone:string;
+    Email:string;
+    Username:string;    
+    Password:string;    
+    Gender : string;     
+    City:string;
+    Country:string;
+    BloodType:string;
+    RhFactor:string;
+    Height:number;    
+    Weight:number;       
+    Doctor : Doctor;
+    Allergies: Array<Allergy>;
+
+}
